e25ca424afed8d5d043a3cae4bc236d2ffe1cae4
Antoni Sierakowski <antonisierakowski@gmail.com>
2019-03-19 17:42:10 +0100

    Updates

---
+++ precache-manifest.a110ce5b88dc521df2c35839ac5d5b17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "965d5f8df3004e393441",
+    "url": "/portfolio/static/js/main.7be07215.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/portfolio/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "965d5f8df3004e393441",
+    "url": "/portfolio/static/css/main.458b0e07.chunk.css"
+  },
+  {
+    "revision": "ddb324999c577865689b4197fa2d88a0",
+    "url": "/portfolio/index.html"
+  }
+];
