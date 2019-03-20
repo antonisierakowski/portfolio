32ca29f8efe65fec7379706246af91a7f3a21081
Antoni Sierakowski <antonisierakowski@gmail.com>
2019-03-20 15:44:30 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a110ce5b88dc521df2c35839ac5d5b17.js"
+  "/portfolio/precache-manifest.a8ced7ff061725e30654bb28493642b8.js"
 );
 
 workbox.clientsClaim();
