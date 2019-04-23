3e0cdfdad10f5c71a2e878615a6e2df2265bb498
Antoni Sierakowski <antonisierakowski@gmail.com>
2019-04-24 00:47:21 +0200

    Updates

---
+++ precache-manifest.4196927e8944fc964e26dbbd4d80ffc0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "3461ec497bc8a6a0f1998318c3790303",
+    "url": "/portfolio/static/media/pokedex.3461ec49.png"
+  },
+  {
+    "revision": "0cb79e43ebbaa9f85553",
+    "url": "/portfolio/static/css/main.3ed8746b.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "625f3922316e2979d492be778b92743a",
+    "url": "/portfolio/static/media/fontisto.625f3922.svg"
+  },
+  {
+    "revision": "3353d680ae5c3df751d2",
+    "url": "/portfolio/static/js/2.a883e9f6.chunk.js"
+  },
+  {
+    "revision": "2773063b8053fcba15677b34ef7895b7",
+    "url": "/portfolio/static/media/portrait.2773063b.png"
+  },
+  {
+    "revision": "6268d13f42fee2f616da73604096c737",
+    "url": "/portfolio/static/media/typescript.6268d13f.svg"
+  },
+  {
+    "revision": "bb59a5f7b2fa5a8841b4765455980c07",
+    "url": "/portfolio/static/media/custom-countdown.bb59a5f7.png"
+  },
+  {
+    "revision": "a480e5531d44aae2f722c6276ad370f2",
+    "url": "/portfolio/static/media/split-the-chores.a480e553.png"
+  },
+  {
+    "revision": "0cb79e43ebbaa9f85553",
+    "url": "/portfolio/static/js/main.8002b348.chunk.js"
+  },
+  {
+    "revision": "52c457c471980ed99b9860d504759a58",
+    "url": "/portfolio/static/media/basic-synth.52c457c4.png"
+  },
+  {
+    "revision": "1c6f0c39ba3f845dbecaea2dde1cad13",
+    "url": "/portfolio/static/media/weather-app.1c6f0c39.png"
+  },
+  {
+    "revision": "7e429ab7b07d9ad4f46ee373735ca788",
+    "url": "/portfolio/static/media/game-of-life.7e429ab7.png"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/portfolio/static/media/fontisto.b49ae8ab.ttf"
+  },
+  {
+    "revision": "5b2dc6fc8ca439534ca2b506569fafc3",
+    "url": "/portfolio/static/media/fontisto.5b2dc6fc.woff"
+  },
+  {
+    "revision": "80bc092bef57222b52e7e3e711b4cdf2",
+    "url": "/portfolio/static/media/fontisto.80bc092b.eot"
+  },
+  {
+    "revision": "3353d680ae5c3df751d2",
+    "url": "/portfolio/static/css/2.1b78d58f.chunk.css"
+  },
+  {
+    "revision": "6343b88b8dc391dac462c2e7b6a287a6",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8a658fe75ac4460a0f086f918c6992ac.js"
+  "/portfolio/precache-manifest.4196927e8944fc964e26dbbd4d80ffc0.js"
 );
 
 workbox.clientsClaim();
